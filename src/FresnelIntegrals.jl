67d265655c5ebe36411cc96d88a3dd367a711f43
Kiran Shila <me@kiranshila.com>
2019-09-30 12:02:16 -0400

    updated Project.toml

--- src/FresnelIntegrals.jl
+++ src/FresnelIntegrals.jl
@@ -1,5 +1,18 @@
 module FresnelIntegrals
+using SpecialFunctions
+export fresnelc
+export fresnels
 
-greet() = print("Hello World!")
+"""
+        fresnelc(z)
+Calculates the Fresnel cosine integral for the number z
+"""
+fresnelc(z::Number) = 0.25*(1-1im)*(1im*erf(0.5*(1-1im)*z*√(π)) + erf(0.5*(1+1im)*z*√(π)))
+
+"""
+        fresnels(z)
+Calculates the Fresnel sine integral for the number z
+"""
+fresnels(z::Number) = 0.25*(1+1im)*(-1im*erf(0.5*(1-1im)*z*√(π)) + erf(0.5*(1+1im)*z*√(π)))
 
 end # module
