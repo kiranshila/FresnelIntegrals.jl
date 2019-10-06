f300f8ffea280339d0e61bd9e3de8f4d5b59285b
Kiran Shila <me@kiranshila.com>
2019-10-06 14:35:10 -0400

    Wrong math in docs

--- src/FresnelIntegrals.jl
+++ src/FresnelIntegrals.jl
@@ -7,14 +7,14 @@ export fresnel
 """
         fresnelc(z)
 Calculates the Fresnel cosine integral for the number z for
-    ``C(z) = \\int_{0}^{\\infty} \\cos{\\left(\\frac{\\pi t^2}{2}\\right)}``
+    ``C(z) = \\int_{0}^{z} \\cos{\\left(\\frac{\\pi t^2}{2}\\right)}dt``
 """
 fresnelc(z::Number) = 0.25*(1-1im)*(1im*erf(0.5*(1-1im)*z*√(π)) + erf(0.5*(1+1im)*z*√(π)))
 
 """
         fresnels(z)
 Calculates the Fresnel sine integral for the number z for
-    ``S(z) = \\int_{0}^{\\infty} \\sin{\\left(\\frac{\\pi t^2}{2}\\right)}``
+    ``S(z) = \\int_{0}^{z} \\sin{\\left(\\frac{\\pi t^2}{2}\\right)}dt``
 """
 fresnels(z::Number) = 0.25*(1+1im)*(-1im*erf(0.5*(1-1im)*z*√(π)) + erf(0.5*(1+1im)*z*√(π)))
 
